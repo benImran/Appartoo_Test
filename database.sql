f43688cdb440458c02af1506a095b4afdeb2d729
benImran <benchaa.said-medjahed@hetic.net>
2017-07-11 02:51:23 +0200

    Fin

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  localhost:3306
--- Généré le :  Mar 11 Juillet 2017 à 00:36
+-- Généré le :  Mar 11 Juillet 2017 à 02:50
 -- Version du serveur :  5.6.35
 -- Version de PHP :  7.0.15
 
@@ -26,7 +26,19 @@ CREATE TABLE `bonobo_friends` (
   `age` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `famille` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
   `race` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `nourriture` varchar(30) COLLATE utf8_unicode_ci NOT NULL
+  `nourriture` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  `user_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `bonobo_friends_bonobo_user`
+--
+
+CREATE TABLE `bonobo_friends_bonobo_user` (
+  `bonobo_friends_id` int(11) NOT NULL,
+  `bonobo_user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -48,7 +60,7 @@ CREATE TABLE `fos_user` (
   `confirmation_token` varchar(180) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password_requested_at` datetime DEFAULT NULL,
   `roles` longtext COLLATE utf8_unicode_ci NOT NULL COMMENT '(DC2Type:array)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=COMPACT;
 
 --
 -- Index pour les tables exportées
@@ -59,6 +71,14 @@ CREATE TABLE `fos_user` (
 --
 ALTER TABLE `bonobo_friends`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `bonobo_friends_bonobo_user`
+--
+ALTER TABLE `bonobo_friends_bonobo_user`
+  ADD PRIMARY KEY (`bonobo_friends_id`,`bonobo_user_id`),
+  ADD KEY `IDX_67939479D9F6E44F` (`bonobo_friends_id`),
+  ADD KEY `IDX_6793947977089779` (`bonobo_user_id`);
 
 --
 -- Index pour la table `fos_user`
@@ -77,9 +97,19 @@ ALTER TABLE `fos_user`
 -- AUTO_INCREMENT pour la table `bonobo_friends`
 --
 ALTER TABLE `bonobo_friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `fos_user`
 --
 ALTER TABLE `fos_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `bonobo_friends_bonobo_user`
+--
+ALTER TABLE `bonobo_friends_bonobo_user`
+  ADD CONSTRAINT `FK_6793947977089779` FOREIGN KEY (`bonobo_user_id`) REFERENCES `fos_user` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_67939479D9F6E44F` FOREIGN KEY (`bonobo_friends_id`) REFERENCES `bonobo_friends` (`id`) ON DELETE CASCADE;
